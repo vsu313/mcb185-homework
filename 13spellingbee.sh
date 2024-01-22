f746f826bae3bc631a77f23591c6d1551f507ea1
vsu313 <135072278+vsu313@users.noreply.github.com>
2024-01-22 11:29:25 -0800

    completed unit 1 hw

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,3 +1,3 @@
-#gunzip -c dictionary.gz | grep "r" | grep -E "[azircon]{4,100}" | grep -v "[^azircon]"
+gunzip -c dictionary.gz | grep "r" | grep -E "[azircon]{4,100}" | grep -v "[^azircon]"
 
 gunzip -c dictionary.gz | grep "r" | grep -v "[bdefghjklmpqstuvwxy]" | grep -E ".{4,100}"
